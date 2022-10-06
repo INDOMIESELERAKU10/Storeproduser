457df49bc3537fc00655a3f0129f4dc6c468b7a6
INDOMIESELERAKU10 <115142310+INDOMIESELERAKU10@users.noreply.github.com>
2022-10-06 15:23:56 +0700

    tugas1
    selesai

---
+++ src/latihan1/latihan1.java
@@ -0,0 +1,31 @@
+package latihan1;
+
+import java.util.Scanner;
+
+public class latihan1 {
+	static String nama;
+	int usia;
+	char inisial;
+	public static String getNama(String inNama){
+		nama=inNama;
+		return nama;
+	}
+	public int getUsia(int inUsia){
+		usia=inUsia;
+		return usia;
+	}
+	public char getinisial(char inInisial){
+		inisial=inInisial;
+		return inisial;
+	}
+	public static void showNama(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("nama :");
+		String valNama=input.nextLine();
+		getNama(valNama);
+	}
+	public static void main (String[]args){
+		showNama();
+		System.out.println("Nama ini "+nama);
+	}
+}
